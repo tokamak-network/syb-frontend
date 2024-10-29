5b70e112b552a47343ae53c0ac72c26cbe3e0cdf
MightyDevKnight <james.mario.bello1209@gmail.com>
2024-10-29 14:06:18 +0100

    Add Wallet Connection on Explore Page

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,11 +1,16 @@
+"use client";
+
 import Button from "@/components/Button";
 import SearchBarComponent from "@/components/SearchBar";
 import ChainActivityTable from "@/components/tables/ChainActivity";
 import UserActivityChart from "@/components/UserAcrivityChart";
 import React from "react";
 import { PiUserCircleFill } from "react-icons/pi";
+import { useWallet } from "@/context/WalletContext";
 
 const ExplorerPage: React.FC = () => {
+  const { account, balance, connectWallet, isMetaMaskInstalled } = useWallet();
+
   return (
     <div className="p-8 space-y-4">
       <div className="flex space-x-3 items-center">
@@ -13,23 +18,29 @@ const ExplorerPage: React.FC = () => {
           placeholder={"Search by address / txn hash / block / token..."}
         />
         <PiUserCircleFill size={39} />
-        <Button className="bg-[#1379FF] text-white rounded-xl font-kanit font-bold">
-          Connect Wallet
-        </Button>
+        {!account ? (
+          <Button
+            onClick={connectWallet}
+            className="bg-[#1379FF] text-white rounded-xl font-kanit font-bold"
+          >
+            {isMetaMaskInstalled ? "Connect Wallet" : "Install MetaMask"}
+          </Button>
+        ) : (
+          <div className="flex flex-col items-start">
+            <span className="text-white font-kanit">{`Account: ${account}`}</span>
+            <span className="text-white font-kanit">{`Balance: ${balance} ETH`}</span>
+          </div>
+        )}
       </div>
       <div className="flex justify-between space-x-10">
         <div className="flex flex-col space-y-14 text-[#9E9EA3] font-narnoor w-full">
           <div className="flex flex-col space-y-14 bg-[#1E1F2090] px-2.5 pt-5 rounded-lg">
-            {" "}
-            {/** Transactions */}
             <span className="text-3xl">Transactions</span>
             <span className="text-xl">
               <span className="text-white text-3xl">1,129,100</span> (24h)
             </span>
           </div>
           <div className="flex flex-col space-y-14 bg-[#1E1F2090] px-2.5 pt-5 rounded-lg">
-            {" "}
-            {/** Pending Transactions */}
             <span className="text-3xl">Pending transactions</span>
             <span className="text-xl">
               <span className="text-white text-3xl">151</span> (24h)
