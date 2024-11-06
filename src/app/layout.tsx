7aaf7267623384ac6820f9b125043ed316599b6d
MightyDevKnight <james.mario.bello1209@gmail.com>
2024-11-06 13:17:02 +0100

    Configure Use Query for Fetch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from 'next';
 
 import React from 'react';
-
 import './globals.css';
 import dynamic from 'next/dynamic';
 
 import { WalletProvider } from '@/context/WalletContext';
+import { ReactQueryProvider } from '@/providers/ReactQueryProvider';
 
 const MainContainer = dynamic(() => import('@/containers/MainContainer'), {
 	ssr: false,
@@ -25,31 +25,33 @@ export default function RootLayout({
 		<html lang="en">
 			<head>
 				<link
-					href="https://fonts.googleapis.com/css2?family=Abhaya+Libre:wght@400;700&Acme:wght@400;700&family=Poppins:wght@400;700&family=Roboto:wght@400;700&family=Kanit:wght@400;700&family=Montserrat:wght@400;700&family=Narnoor:wght@400;700&display=swap"
+					href="https://fonts.googleapis.com/css2?family=Abhaya+Libre:wght@400;700&family=Acme:wght@400;700&family=Poppins:wght@400;700&family=Roboto:wght@400;700&family=Kanit:wght@400;700&family=Montserrat:wght@400;700&family=Narnoor:wght@400;700&display=swap"
 					rel="stylesheet"
 				/>
 			</head>
-			<WalletProvider>
-				<body
-					style={{
-						backgroundImage: 'url(/images/back0.png)',
-						backgroundSize: 'cover',
-						backgroundPosition: 'center',
-						minHeight: '100vh',
-					}}
-				>
-					<div
-						style={{
-							backgroundImage: 'url(/images/back1.png)',
-							backgroundSize: 'cover',
-							backgroundPosition: 'center',
-							height: '1000px',
-						}}
-					>
-						<MainContainer>{children}</MainContainer>
-					</div>
-				</body>
-			</WalletProvider>
+			<body
+				style={{
+					backgroundImage: 'url(/images/back0.png)',
+					backgroundSize: 'cover',
+					backgroundPosition: 'center',
+					minHeight: '100vh',
+				}}
+			>
+				<WalletProvider>
+					<ReactQueryProvider>
+						<div
+							style={{
+								backgroundImage: 'url(/images/back1.png)',
+								backgroundSize: 'cover',
+								backgroundPosition: 'center',
+								height: '1000px',
+							}}
+						>
+							<MainContainer>{children}</MainContainer>
+						</div>
+					</ReactQueryProvider>
+				</WalletProvider>
+			</body>
 		</html>
 	);
 }
---
+++ src/hooks/useFetchUsers.ts
@@ -0,0 +1,17 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { User } from '@/types';
+
+const fetchUsers = async (): Promise<User[]> => {
+	const response = await fetch('/api/users'); // example endpoint to fetch users
+
+	if (!response.ok) {
+		throw new Error('Network response was not ok');
+	}
+
+	return response.json();
+};
+
+export const useFetchUsers = () => {
+	return useQuery<User[], Error>('users', fetchUsers);
+};
