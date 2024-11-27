4e8519772543e5eb97c6a601eca7d00281bc2515
MightyDevKnight <james.mario.bello1209@gmail.com>
2024-11-27 12:35:41 +0100

    Fix Table Colors

--- src/components/tables/ChainActivity.tsx
+++ src/components/tables/ChainActivity.tsx
@@ -16,6 +16,7 @@ const ChainActivityTable: React.FC = () => {
 
 	const filteredData = explorerData.filter((transaction) => {
 		const query = searchQuery.toLowerCase();
+
 		return (
 			transaction.txHash.toLocaleLowerCase().includes(query) ||
 			transaction.txUser.from.toLowerCase().includes(query) ||
@@ -32,19 +33,19 @@ const ChainActivityTable: React.FC = () => {
 			/>
 			<div className="flex justify-between">
 				<div className="flex justify-between space-x-3 font-narnoor">
-					<Button className="bg-[#1B202C] text-center">Validated</Button>
-					<Button className="bg-[#101112]">Pending</Button>
+					<Button className="bg-tableBackground text-center">Validated</Button>
+					<Button className="bg-tableBackground">Pending</Button>
 				</div>
 				<div className="flex space-x-2 font-narnoor">
-					<Button className="border-2 border-[#23323E] bg-[#1B202C] text-center text-[#6290B6]">
+					<Button className="border-tableBorder bg-tableBackground border-2 text-center text-[#6290B6]">
 						First
 					</Button>
 					<div className="flex space-x-3">
-						<Button className="border-2 border-[#23323E] bg-[#101112] text-[#6290B6]">
+						<Button className="border-tableBorder bg-tableBackground border-2 text-[#6290B6]">
 							{'<'}
 						</Button>
 						<Button className="bg-[#4B5569]">{'1'}</Button>
-						<Button className="border-2 border-[#23323E] bg-[#101112] text-[#6290B6]">
+						<Button className="border-tableBorder bg-tableBackground border-2 text-[#6290B6]">
 							{'>'}
 						</Button>
 					</div>
@@ -52,7 +53,7 @@ const ChainActivityTable: React.FC = () => {
 			</div>
 			<div className="overflow-x-auto rounded-lg shadow-lg">
 				<table className="min-w-full table-auto rounded-lg">
-					<thead className="bg-[#232425] font-abhaya text-[#B0B0B0]">
+					<thead className="bg-tableHeader text-tableTextPrimary font-abhaya">
 						<tr>
 							<th className="px-6 py-3 text-left text-sm font-bold uppercase tracking-wider">
 								Transaction Hash
@@ -77,32 +78,34 @@ const ChainActivityTable: React.FC = () => {
 							</th>
 						</tr>
 					</thead>
-					<tbody className="bg-[#101112] font-abhaya">
+					<tbody className="bg-tableBackground font-abhaya">
 						<tr>
-							<td className="bg-[#353025] px-6 py-1" colSpan={7}>
-								<span className="text-[#4897EF]">9903 more transactions </span>
-								<span className="text-[#D7BC90]">have come in</span>
+							<td className="bg-tableRowBackground px-6 py-1" colSpan={7}>
+								<span className="text-tableTextInfo">
+									9903 more transactions{' '}
+								</span>
+								<span className="text-tableTextNotice">have come in</span>
 							</td>
 						</tr>
 						{filteredData.map((transaction, index) => (
 							<tr
 								key={index}
-								className={`border-b-2 border-[#232425] bg-[#101112] font-abhaya text-gray-700 transition-colors duration-300 hover:bg-[#26292c]`}
+								className={`bg-tableBackground border-tableBorder hover:bg-tableHover border-b-2 font-abhaya text-gray-700 transition-colors duration-300`}
 							>
 								<td className="flex flex-col space-y-4 whitespace-nowrap px-6 py-2 text-left font-normal">
-									<span className="text-[#70B4E8]">{`0xHash${index}`}</span>
-									<span className="text-[#FFFFFF]">{'1m ago'}</span>
+									<span className="text-tableTextSecondary">{`0xHash${index}`}</span>
+									<span className="text-white">{'1m ago'}</span>
 								</td>
 								<td className="space-y-2 whitespace-nowrap px-6 py-2 text-left font-normal">
 									<TxTypes txType={transaction.type.txType} />
 									<TxStatus status={transaction.type.txStatus} />
 								</td>
 								<td className="whitespace-nowrap px-6 py-2 text-left font-normal">
-									<span className="rounded-lg bg-[#1E1F20] px-2 py-1 font-abhaya text-[#E6D7BE]">
+									<span className="bg-tableButtonBackground text-tableButtonText rounded-lg px-2 py-1 font-abhaya">
 										{transaction.method.toLocaleLowerCase()}
 									</span>
 								</td>
-								<td className="whitespace-nowrap px-6 py-2 text-left font-normal text-[#70B4E8]">
+								<td className="text-tableTextSecondary whitespace-nowrap px-6 py-2 text-left font-normal">
 									{transaction.blockNumber}
 								</td>
 								<td className="flex whitespace-nowrap px-6 py-2 text-left font-normal">
