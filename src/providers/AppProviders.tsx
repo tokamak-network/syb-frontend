687ca6079d8f8b751d63dd91f6591df9fd6d7773
MightyDevKnight <james.mario.bello1209@gmail.com>
2024-11-30 16:52:25 +0100

    Add Wagmi Provider

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -1,4 +1,9 @@
+'use client';
+
 import React from 'react';
+import { createConfig, http, WagmiProvider } from 'wagmi';
+import { mainnet, sepolia } from 'wagmi/chains';
+import { metaMask } from 'wagmi/connectors';
 
 import { WalletProvider, ToastProvider } from '@/context';
 import { ReactQueryProvider } from '@/providers/ReactQueryProvider';
@@ -7,12 +12,23 @@ interface AppProvidersProps {
 	children: React.ReactNode;
 }
 
+export const config = createConfig({
+	chains: [sepolia, mainnet],
+	connectors: [metaMask()],
+	transports: {
+		[sepolia.id]: http(),
+		[mainnet.id]: http(),
+	},
+});
+
 export const AppProviders: React.FC<AppProvidersProps> = ({ children }) => {
 	return (
-		<WalletProvider>
-			<ToastProvider>
-				<ReactQueryProvider>{children}</ReactQueryProvider>
-			</ToastProvider>
-		</WalletProvider>
+		<WagmiProvider config={config}>
+			<WalletProvider>
+				<ToastProvider>
+					<ReactQueryProvider>{children}</ReactQueryProvider>
+				</ToastProvider>
+			</WalletProvider>
+		</WagmiProvider>
 	);
 };
