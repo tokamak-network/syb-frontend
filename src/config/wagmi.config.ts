25799123d13255820439f138070ec922323ca771
MightyDevKnight <james.mario.bello1209@gmail.com>
2024-11-30 19:14:47 +0100

    Wallet Hook

--- src/config/wagmi.config.ts
+++ src/config/wagmi.config.ts
@@ -1,10 +1,10 @@
-import { http, createConfig } from '@wagmi/core'
-import { mainnet, sepolia } from '@wagmi/core/chains'
+import { http, createConfig } from '@wagmi/core';
+import { mainnet, sepolia } from '@wagmi/core/chains';
 
 export const config = createConfig({
-  chains: [mainnet, sepolia],
-  transports: {
-    [mainnet.id]: http(),
-    [sepolia.id]: http(),
-  },
-})
+	chains: [mainnet, sepolia],
+	transports: {
+		[mainnet.id]: http(),
+		[sepolia.id]: http(),
+	},
+});
--- src/hooks/useWallet.ts
+++ src/hooks/useWallet.ts
@@ -2,48 +2,54 @@ import { config } from '@/config';
 import { getBalance } from '@wagmi/core';
 import { ethers } from 'ethers';
 import { useState, useEffect } from 'react';
-import { useAccount, useConnect, useDisconnect, useEnsAvatar, useEnsName } from 'wagmi';
+import {
+	useAccount,
+	useConnect,
+	useDisconnect,
+	useEnsAvatar,
+	useEnsName,
+} from 'wagmi';
 
 export const useWallet = () => {
-  const { address, isConnected, chain, chainId } = useAccount();
-  const { disconnect } = useDisconnect();
-  const { connectors, connect } = useConnect();
-  const { data: ensName } = useEnsName({ address });
-  const { data: ensAvatar } = useEnsAvatar({ name: ensName! });
+	const { address, isConnected, chain, chainId } = useAccount();
+	const { disconnect } = useDisconnect();
+	const { connectors, connect } = useConnect();
+	const { data: ensName } = useEnsName({ address });
+	const { data: ensAvatar } = useEnsAvatar({ name: ensName! });
 
-  const [balance, setBalance] = useState<string | null>(null);
+	const [balance, setBalance] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchBalance = async () => {
-      if (address) {
-        try {
-          const balanceResult = await getBalance(config, {
-            address: address,
-            unit: 'ether',
-          });
-          setBalance(ethers.utils.formatEther(balanceResult.value));
-        } catch (error) {
-          console.error('Error fetching balance:', error);
-          setBalance(null);
-        }
-      } else {
-        setBalance(null);
-      }
-    };
+	useEffect(() => {
+		const fetchBalance = async () => {
+			if (address) {
+				try {
+					const balanceResult = await getBalance(config, {
+						address: address,
+						unit: 'ether',
+					});
+					setBalance(ethers.utils.formatEther(balanceResult.value));
+				} catch (error) {
+					console.error('Error fetching balance:', error);
+					setBalance(null);
+				}
+			} else {
+				setBalance(null);
+			}
+		};
 
-    fetchBalance();
-  }, [address]);
+		fetchBalance();
+	}, [address]);
 
-  return {
-    address,
-    balance,
-    isConnected,
-    chain,
-    chainId,
-    connectors,
-    ensName,
-    ensAvatar,
-    disconnect,
-    connect,
-  };
+	return {
+		address,
+		balance,
+		isConnected,
+		chain,
+		chainId,
+		connectors,
+		ensName,
+		ensAvatar,
+		disconnect,
+		connect,
+	};
 };
