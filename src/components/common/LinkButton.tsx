cc3e2d41e59c84ed48a1a819fd023c92b74dcd65
MightyDevKnight <james.mario.bello1209@gmail.com>
2024-12-06 10:15:17 +0100

    Update LinkButton Style

--- src/components/common/LinkButton.tsx
+++ src/components/common/LinkButton.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { IconType } from 'react-icons';
 
 import { cn } from '@/utils/cn';
+import { useTheme } from '@/context/ThemeContext';
 
 import { Button } from './Button';
 
@@ -21,10 +22,21 @@ export const LinkButton: React.FC<LinkButtonProps> = ({
 	onMouseEnter,
 	...props
 }) => {
+	const { theme } = useTheme();
+
+	const themeStyles = {
+		light: 'before:bg-black after:bg-black',
+		dark: 'before:bg-white after:bg-white',
+		dim: 'before:bg-gray-100 after:bg-gray-100',
+	};
+
+	const currentThemeStyles = themeStyles[theme];
+
 	return (
 		<Button
 			className={cn(
 				'link-button relative flex w-max overflow-hidden px-0 pb-1 before:bg-white after:bg-white hover:bg-inherit',
+				currentThemeStyles,
 				className,
 			)}
 			onClick={() => {
