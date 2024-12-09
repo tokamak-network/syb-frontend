74a69b4c92027e91bb1e3dcde4d5199aa74a4ec6
MightyDevKnight <james.mario.bello1209@gmail.com>
2024-12-09 16:36:56 +0100

    Add Block and Transaction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,18 +8,32 @@ import { UserActivityLineChart, ChainActivityTable } from '@/components';
 const HomePage: React.FC = () => {
 	return (
 		<div className="flex flex-col items-center px-20">
-			<div className="flex w-full justify-between">
+			<div className="flex w-full justify-between space-x-5">
 				<div className="flex w-full flex-col space-y-14 font-narnoor">
 					<div className="flex flex-col space-y-14 rounded-lg border-2 border-tableBorder px-2.5 pt-5">
-						<p className="text-3xl text-primaryText">Transactions</p>
-						<p className="text-xl text-secondaryText">
+						<p className="text-primaryText text-3xl">Transactions</p>
+						<p className="text-secondaryText text-xl">
 							<span className="text-3xl">1,129,100</span> (24h)
 						</p>
 					</div>
 					<div className="flex flex-col space-y-14 rounded-lg border-2 border-tableBorder px-2.5 pt-5">
-						<p className="text-3xl text-primaryText">Pending Transactions</p>
-						<p className="text-xl text-secondaryText">
+						<p className="text-primaryText text-3xl">Pending Transactions</p>
+						<p className="text-secondaryText text-xl">
 							<span className="text-3xl">151</span> (24h)
+						</p>
+					</div>
+				</div>
+				<div className="flex w-full flex-col space-y-14 font-narnoor">
+					<div className="flex flex-col space-y-14 rounded-lg border-2 border-tableBorder px-2.5 pt-5">
+						<p className="text-primaryText text-3xl">Last Block</p>
+						<p className="text-secondaryText text-xl">
+							<span className="text-3xl">#12,345,678</span> (2 mins ago)
+						</p>
+					</div>
+					<div className="flex flex-col space-y-14 rounded-lg border-2 border-tableBorder px-2.5 pt-5">
+						<p className="text-primaryText text-3xl">Last Transaction</p>
+						<p className="text-secondaryText text-xl">
+							<span className="text-3xl">0x123...abcd</span> (5 ETH)
 						</p>
 					</div>
 				</div>
