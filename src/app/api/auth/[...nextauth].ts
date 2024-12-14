c9e7edc86ab1f81aa446751a5d02191d79477d24
MightyDevKnight <james.mario.bello1209@gmail.com>
2024-12-14 17:40:52 +0100

    Add AuthOption Configuration

--- src/app/api/auth/[...nextauth].ts
+++ src/app/api/auth/[...nextauth].ts
@@ -1,10 +1,47 @@
-import NextAuth from 'next-auth';
+import NextAuth, { AuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import GoogleProvider from 'next-auth/providers/google';
 import GitHubProvider from 'next-auth/providers/github';
 import EmailProvider from 'next-auth/providers/email';
+import bcrypt from 'bcrypt';
 
-export const authOptions = {
+import prisma from '@/lib/prisma';
+
+export const authOptions: AuthOptions = {
+	adapter: PrismaAdapter(prisma),
 	providers: [
+		CredentialsProvider({
+			name: 'Credentials',
+			credentials: {
+				email: { label: 'Email', type: 'text' },
+				password: { label: 'Password', type: 'password' },
+			},
+			async authorize(credentials) {
+				if (!credentials?.email || !credentials?.password) {
+					throw new Error('Email and password are required');
+				}
+
+				const user = await prisma.user.findUnique({
+					where: { email: credentials.email },
+				});
+
+				if (!user) {
+					throw new Error('No user found with this email');
+				}
+
+				const isValidPassword = await bcrypt.compare(
+					credentials.password,
+					user.password,
+				);
+
+				if (!isValidPassword) {
+					throw new Error('Invalid password');
+				}
+
+				return user;
+			},
+		}),
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID!,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
@@ -25,7 +62,27 @@ export const authOptions = {
 			from: process.env.EMAIL_FROM!,
 		}),
 	],
+	session: {
+		strategy: 'jwt',
+	},
 	secret: process.env.NEXTAUTH_SECRET,
+	callbacks: {
+		async jwt({ token, user }) {
+			if (user) {
+				token.id = user.id;
+			}
+
+			return token;
+		},
+		async session({ session, token }) {
+			if (token) {
+				session.user = session.user || {};
+				session.user.id = token.id as string;
+			}
+
+			return session;
+		},
+	},
 };
 
 const handler = NextAuth(authOptions);
---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth/next";
+
+declare module 'next-auth' {
+	interface Session {
+		user: {
+			id: string;
+			name?: string | null;
+			email?: string | null;
+			image?: string | null;
+		};
+	}
+}
