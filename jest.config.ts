7b481db898f8cf6014a6e4954dca7c3ddf7e2cd0
MightyDevKnight <james.mario.bello1209@gmail.com>
2025-01-15 22:07:16 +0100

    Update Jest Config for Name Mapping

--- jest.config.ts
+++ jest.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  }
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
