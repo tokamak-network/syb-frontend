7fa2370418da1cb7c1150cf151857b3e6d22dd7a
MightyDevKnight <james.mario.bello1209@gmail.com>
2025-02-06 03:42:18 +0100

    Add Input Component

---
+++ src/components/common/Input.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import React from 'react';
+
+import { cn } from '@/utils/cn';
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+	label?: string;
+	className?: string;
+	labelClassName?: string;
+}
+
+export const Input: React.FC<InputProps> = ({
+	label,
+	className,
+	labelClassName,
+	...props
+}) => {
+	return (
+		<div className="flex flex-col space-y-2">
+			{label && (
+				<label
+					className={cn('text-sm font-medium text-gray-700', labelClassName)}
+					htmlFor={props.id}
+				>
+					{label}
+				</label>
+			)}
+			<input
+				className={cn(
+					'w-full rounded-md border border-gray-300 p-2 text-sm shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50',
+					className,
+				)}
+				{...props}
+			/>
+		</div>
+	);
+};
