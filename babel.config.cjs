73ad9dfae16df0f15fb96e1b58683b4b5cf452eb
MightyDevKnight <james.mario.bello1209@gmail.com>
2025-05-21 18:48:29 +0200

    Refactor: Fix Prettier Issue

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,24 +1,27 @@
 module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' }, modules: 'commonjs' }],
-    '@babel/preset-react',
-    '@babel/preset-typescript',
-  ],
-  plugins: [],
-  overrides: [
-    {
-      test: /node_modules\/(wagmi|viem|@wagmi|@tanstack\/react-query)/,
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            targets: {
-              node: 'current',
-            },
-            modules: 'commonjs',
-          },
-        ],
-      ],
-    },
-  ],
+	presets: [
+		[
+			'@babel/preset-env',
+			{ targets: { node: 'current' }, modules: 'commonjs' },
+		],
+		'@babel/preset-react',
+		'@babel/preset-typescript',
+	],
+	plugins: [],
+	overrides: [
+		{
+			test: /node_modules\/(wagmi|viem|@wagmi|@tanstack\/react-query)/,
+			presets: [
+				[
+					'@babel/preset-env',
+					{
+						targets: {
+							node: 'current',
+						},
+						modules: 'commonjs',
+					},
+				],
+			],
+		},
+	],
 };
--- jest.config.cjs
+++ jest.config.cjs
@@ -1,14 +1,14 @@
 module.exports = {
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.(t|j)sx?$': 'babel-jest',
-  },
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
-  transformIgnorePatterns: [
-    // This is critical - make all node_modules transform except specific ones that aren't ESM
-    '/node_modules/(?!(wagmi|@wagmi|viem|@tanstack/react-query)/)',
-  ],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+	testEnvironment: 'jsdom',
+	transform: {
+		'^.+\\.(t|j)sx?$': 'babel-jest',
+	},
+	moduleNameMapper: {
+		'^@/(.*)$': '<rootDir>/src/$1',
+	},
+	transformIgnorePatterns: [
+		// This is critical - make all node_modules transform except specific ones that aren't ESM
+		'/node_modules/(?!(wagmi|@wagmi|viem|@tanstack/react-query)/)',
+	],
+	setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
