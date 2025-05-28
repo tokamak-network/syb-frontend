419ce290b8f62c1c877c9238d7ab4026e3e34cb8
MightyDevKnight <james.mario.bello1209@gmail.com>
2025-05-28 14:22:40 +0200

    Refactor: Prettier Issue

--- next.config.js
+++ next.config.js
@@ -1,31 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['gateway.pinata.cloud'],
-  },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/home',
-        permanent: true,
-      }
-    ]
-  },
-  async rewrites() {
-    const apiHost = process.env.NEXT_PUBLIC_API_HOST || 'http://localhost';
-    const apiPort = process.env.NEXT_PUBLIC_API_PORT || '8080';
+	images: {
+		domains: ['gateway.pinata.cloud'],
+	},
+	async redirects() {
+		return [
+			{
+				source: '/',
+				destination: '/home',
+				permanent: true,
+			},
+		];
+	},
+	async rewrites() {
+		const apiHost = process.env.NEXT_PUBLIC_API_HOST || 'http://localhost';
+		const apiPort = process.env.NEXT_PUBLIC_API_PORT || '8080';
 
-    return [
-      {
-        source: '/sequencer/:path*',
-        destination: `${apiHost}:${apiPort}/api/v1/:path*`,
-      },
-    ];
-  },
-  experimental: {
-    forceSwcTransforms: true,
-  },
+		return [
+			{
+				source: '/sequencer/:path*',
+				destination: `${apiHost}:${apiPort}/api/v1/:path*`,
+			},
+		];
+	},
+	experimental: {
+		forceSwcTransforms: true,
+	},
 };
 
 module.exports = nextConfig;
